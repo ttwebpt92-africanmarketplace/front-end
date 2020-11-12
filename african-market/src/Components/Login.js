0e189a01ca560fda999b56c45520407c76f62b33
Amanda Nelson <amanda.gaylord@gmail.com>
2020-11-12 18:53:40 -0500

    PublicHeader and PublicFooter also added. Home.js styled Login.js still bare for now.

--- african-market/src/Components/Login.js
+++ african-market/src/Components/Login.js
@@ -1,33 +1,36 @@
-import React from 'react';
-import { Button, Form, FormGroup, Label, Input, FormFeedback } from 'reactstrap';
+import React from "react";
+import {
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  FormFeedback,
+} from "reactstrap";
 
 function Login() {
-    const onChange = () => {
+  const onChange = () => {};
+  const onSubmit = () => {};
 
-    }
-    const onSubmit = () => {
-
-    }
-
-    return (
-      <Form>
-        <FormGroup>
-          <Label for="username">Username</Label>
-          <Input id="username" onChange={onChange} />
-          <FormFeedback invalid>
-            We do not have record of this email address on file.
-          </FormFeedback>
-        </FormGroup>
-        <FormGroup>
-          <Label for="password">Password</Label>
-          <Input type="password" id="password" onChange={onChange} />
-          <FormFeedback invalid>
-           This password does not match what is on file.
-          </FormFeedback>
-        </FormGroup>
-        <Button onChange={onSubmit} >Login</Button>
-      </Form>
-    );
+  return (
+    <Form>
+      <FormGroup>
+        <Label for="username">Username</Label>
+        <Input id="username" onChange={onChange} />
+        <FormFeedback invalid>
+          We do not have record of this email address on file.
+        </FormFeedback>
+      </FormGroup>
+      <FormGroup>
+        <Label for="password">Password</Label>
+        <Input type="password" id="password" onChange={onChange} />
+        <FormFeedback invalid>
+          This password does not match what is on file.
+        </FormFeedback>
+      </FormGroup>
+      <Button onChange={onSubmit}>Login</Button>
+    </Form>
+  );
 }
 
 export default Login;
