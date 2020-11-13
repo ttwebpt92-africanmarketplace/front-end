50a857eee2803db5e20cf6f753ae9ff7a80dc0ae
Sean Kramer <seandominickramer@gmail.com>
2020-11-12 21:30:04 -0500

    Context/basic setup (#6)
    * Add changes
    
    * Comment out things that are preventing compiling

--- african-market/src/Components/Dashboard.js
+++ african-market/src/Components/Dashboard.js
@@ -1,20 +1,9 @@
-<<<<<<< HEAD
-//Visible to users only after they are logged in.
-import React from "react";
-
-function Dashboard() {
-    return (
-        <>
-        </>
-    )
-=======
 import React from 'react';
 
 const Dashboard = () => {
   return (
     <div>Dashboard</div>
   )
->>>>>>> 7ecad3ec8dab91a100774a6b1c41e10b5b7aff4c
 }
 
 export default Dashboard;
