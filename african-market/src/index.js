45dae80f8ea37b9f0797ccba5f49ff49282c908b
Sean Kramer <seandominickramer@gmail.com>
2020-11-12 21:04:29 -0500

    Add changes

--- african-market/src/index.js
+++ african-market/src/index.js
@@ -1,14 +1,5 @@
-<<<<<<< HEAD
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
-=======
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './Components/App'
->>>>>>> 05338f043df34980414dc566a0c826929f306060
 
 ReactDOM.render(<App />, document.querySelector('#root'));
