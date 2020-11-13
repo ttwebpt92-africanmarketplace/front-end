e4ece54d47cce67f4a17fed2b2bde851346ae6cc
Amanda Nelson <amanda.gaylord@gmail.com>
2020-11-12 22:02:04 -0500

    app.css edited for UI

--- african-market/src/Components/App.js
+++ african-market/src/Components/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
-import PrivateRoute from './PrivateRoute';
-import Home from './Home';
-import Login from './Login';
-import Dashboard from './Dashboard';
+import PrivateRoute from "./PrivateRoute";
+import Home from "./Home";
+import Login from "./Login";
+import Dashboard from "./Dashboard";
+import PublicHeader from "./PublicHeader";
+import PublicFooter from "./PublicFooter";
 
 function App() {
   return (
     <div>
-    <div>
-      African Market
+      <PublicHeader />
+      <Router>
+        <Route exact path="/" component={Home} />
+        <Route path="/login" component={Login} />
+        <PrivateRoute exact path="/dashboard" component={Dashboard} />
+      </Router>
+      <PublicFooter />
     </div>
-    <Router>
-      <Route  exact path='/' component={Home} />
-      <Route path='/login' component={Login} />
-      <PrivateRoute exact path='/dashboard' component={Dashboard} />
-    </Router>
-    </div>
-
-  
   );
 }
 
