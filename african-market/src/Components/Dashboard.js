aa5aeedb599fe2f3847b746d11ee2aff9951b2d1
Sean Kramer <seandominickramer@gmail.com>
2020-11-16 21:18:52 -0500

    Add history.push to login, and get request for data in Dashboard

--- african-market/src/Components/Dashboard.js
+++ african-market/src/Components/Dashboard.js
@@ -1,5 +1,7 @@
 //Visible to users only after they are logged in.
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
 import ProductCard from "./ProductCard";
 import styled from "styled-components";
 
@@ -7,6 +9,27 @@ const DashboardDiv = styled.div`
   background-color: gray;
 `;
 function Dashboard() {
+
+const username = "User";
+
+const [itemData, setItemData] = useState();
+
+const getData = () => {
+  axiosWithAuth()
+  .get('/api/items')
+  .then((res) => {
+    console.log('Dashboard res: ', res)
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+}
+
+useEffect(() => {
+  getData()
+}, [])
+
+
   return (
     <DashboardDiv>
       <p>Welcome to your Dashboard {username}!</p>
